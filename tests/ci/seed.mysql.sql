10c1abaa17eb40c351808b68a3126972e7bab05a
shipperizer <alexcabb@gmail.com>
2023-09-25 16:51:55 +0100

    ci: add config files and seed file for ci

---
+++ tests/ci/seed.mysql.sql
@@ -0,0 +1,17 @@
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('superheros', 5501);
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('svcaccts', 5502);
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('civilians', 5503);
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('caped', 5504);
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('lovesailing', 5505);
+INSERT INTO glauth.groups(name, gidnumber) VALUES ('smoker', 5506);
+
+INSERT INTO glauth.includegroups(parentgroupid, includegroupid) VALUES (5503, 5501);
+INSERT INTO glauth.includegroups(parentgroupid, includegroupid) VALUES (5504, 5502);
+INSERT INTO glauth.includegroups(parentgroupid, includegroupid) VALUES (5504, 5501);
+
+INSERT INTO glauth.users(name, uidnumber, primarygroup, passsha256) VALUES ('hackers', 5001, 5501,'6478579e37aff45f013e14eeb30b3cc56c72ccdc310123bcdf53e0333e3f416a');
+INSERT INTO glauth.users(name, uidnumber, primarygroup, passsha256) VALUES ('johndoe', 5002, 5502,'6478579e37aff45f013e14eeb30b3cc56c72ccdc310123bcdf53e0333e3f416a');
+INSERT INTO glauth.users(name, mail, uidnumber, primarygroup, passsha256) VALUES ('serviceuser', 'serviceuser@example.com', 5003, 5502,'652c7dc687d98c9889304ed2e408c74b611e86a40caa51c4b43f1dd5913c5cd0');
+INSERT INTO glauth.users(name, uidnumber, primarygroup, passsha256, othergroups, custattr) VALUES ('user4', 5004, 5504,'652c7dc687d98c9889304ed2e408c74b611e86a40caa51c4b43f1dd5913c5cd0','5505,5506','{"employeetype":["Intern","Temp"],"employeenumber":[12345,54321]}');
+INSERT INTO glauth.capabilities(userid, action, object) VALUES (5001, 'search', 'ou=superheros,dc=glauth,dc=com');
+INSERT INTO glauth.capabilities(userid, action, object) VALUES (5003, 'search', '*');
